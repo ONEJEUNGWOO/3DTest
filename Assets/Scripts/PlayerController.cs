606737620996fadb20b61873bca30c21da2fdd42
원정우 <rjstn155@naver.com>
2025-08-29 03:35:12 +0900

    [Feat] 키보드 입력에 따라 캐릭터 로테이션 조절 로직 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,6 +31,19 @@ public class PlayerController : MonoBehaviour
         direction.y = rigidbody.velocity.y;
 
         rigidbody.velocity = direction;
+
+        Vector3 moveDirection = new Vector3(currentInput.x / 2, 0f, currentInput.y / 2);
+
+        // 입력이 있는 경우에만 회전
+        if (moveDirection.magnitude > 0.01f)
+        {
+            // 이동 방향으로 회전 목표값 계산
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+            // 현재 회전에서 목표 회전으로 천천히 회전
+            float rotationSpeed = 2.5f; // 회전 속도 조절
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 
     public void OnMovement(InputAction.CallbackContext context)
