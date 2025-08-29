4b36a13b34d0a20e152616006455d986789baad6
원정우 <rjstn155@naver.com>
2025-08-30 04:20:50 +0900

    [Feat] 적 이동 애니메이션(test용) 연결 및 NavMesh 자동 추적 기능 구현

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform target;
+    private NavMeshAgent agent;
     public int hp;
 
-    private Rigidbody _rigidbody;
-    private Vector3 changeValue;
+    private Vector3 distance = new Vector3(0, 0, -1);
 
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    public void TakeDamage(int damage, Vector3 knockBackDir, int knockBackForce )
+    private void Update()
+    {
+        agent.SetDestination(target.position - distance);
+
+        //반대방향 보게 하는 법
+        //Vector3 dir = (target.position - transform.position).normalized;
+        //Quaternion targetRot = Quaternion.LookRotation(-dir);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * 5f);
+        
+        transform.LookAt(target);
+
+        if (hp != 0) return;
+
+        Dead();
+    }
+
+    public void TakeDamage(int damage, Vector3 knockBackDir, int knockBackForce)
     {
         hp -= damage;
         knockBackDir.y = 0;
@@ -22,6 +40,14 @@ public class Enemy : MonoBehaviour
         StartCoroutine(KnockBackCoroutine(knockBackDir, knockBackForce, 1f));
         //_rigidbody.velocity += knockBackDir * knockBackForce;
         //_rigidbody.AddForce(knockBackDir * knockBackForce, ForceMode.Impulse);
+    }
+
+    public void Dead()
+    {
+        if (hp == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     IEnumerator KnockBackCoroutine(Vector3 dir, float force, float duration)
@@ -38,7 +64,7 @@ public class Enemy : MonoBehaviour
             float easedOut = 1 - Mathf.Pow(1 - elapsed, 3);
 
             transform.position = Vector3.Lerp(startPos, targetPos, easedOut / duration);
-            
+
             yield return null;
         }
 
